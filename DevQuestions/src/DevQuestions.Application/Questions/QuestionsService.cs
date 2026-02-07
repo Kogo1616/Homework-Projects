f15b362d4c8567a7e6ea862523cc4d28ca719195
saba1616 <sabakoghuashvili16@gmail.com>
2026-02-08 00:02:35 +0400

    Fix misplaced closing brace in QuestionsService class
    Moved the closing brace of AddAnswer method to the same line as the class's closing brace, resulting in an extra closing brace. This may cause a syntax error due to mismatched braces.

--- DevQuestions/src/DevQuestions.Application/Questions/QuestionsService.cs
+++ DevQuestions/src/DevQuestions.Application/Questions/QuestionsService.cs
@@ -88,5 +88,5 @@ public class QuestionsService(
     public async Task AddAnswer(Guid questionId, AddAnswerRequest answerRequest,
         CancellationToken cancellationToken)
     {
-    }
+    } 
 }
